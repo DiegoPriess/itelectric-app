39449f7e48d31ee17b0cf7d4db1cd42acdb61c5f
Diego Priess <diegopriess.dev@gmail.com>
2024-11-05 23:18:53 -0300

    Ajustando material-list

--- src/app/component/material/material-list/material-list.component.ts
+++ src/app/component/material/material-list/material-list.component.ts
@@ -1,12 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { Router } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 import { IMaterial } from '../../../core/models/Material';
 import { MaterialService } from '../../../core/services/material.service';
@@ -15,8 +14,6 @@ import { MaterialCreateComponent } from '../material-create/material-create.comp
 import { MaterialEditComponent } from '../material-edit/material-edit.component';
 import { MaterialViewComponent } from '../material-view/material-view.component';
 import { UtilsService } from '../../../core/services/utils.service';
-import { MatLabel } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 @Component({
 	selector: 'app-material-list',
@@ -27,23 +24,18 @@ import { MatInputModule } from '@angular/material/input';
 		MatTableModule,
 		MatIconModule,
 		MatToolbarModule,
-		MatCard,
-		MatCardActions,
-		MatCardContent,
 		MatInputModule,
-		MatLabel
 	],
 	templateUrl: './material-list.component.html',
 	styleUrls: ['./material-list.component.scss']
 })
 export class MaterialListComponent implements OnInit {
-	@Input() selectable: boolean = false;
-	dataSource: IMaterial[] = [];
+	dataSource = new MatTableDataSource<IMaterial>();
 	totalElements: number = 0;
 	pageSize = 10;
 	pageIndex = 0;
-	selectedItems: IMaterial[] = [];
 	searchQuery: string = '';
+	displayedColumns: string[] = ['name', 'price', 'quantityUnitMeasure', 'actions'];
 
 	@Output() add = new EventEmitter<void>();
 	@Output() edit = new EventEmitter<void>();
@@ -58,21 +50,17 @@ export class MaterialListComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.loadData();
+		this.dataSource.paginator = this.paginator;
 	}
 
 	loadData(): void {
-		console.log(`Searching for: ${this.searchQuery}`); // Adicione este log
 		this.materialService.list(this.pageIndex, this.pageSize, this.searchQuery).subscribe({
 			next: (page: Page<IMaterial>) => {
-				this.dataSource = page.content;
+				this.dataSource.data = page.content;
 				this.totalElements = page.totalElements;
-			},
-			error: (err) => {
-				console.error('Erro ao carregar os materiais', err);
 			}
 		});
 	}
-
 
 	onPageChange(event: any): void {
 		this.pageIndex = event.pageIndex;
@@ -87,27 +75,16 @@ export class MaterialListComponent implements OnInit {
 		this.loadData();
 	}
 
-	toggleSelection(item: IMaterial): void {
-		if (this.selectable) {
-			const index = this.selectedItems.findIndex(selected => selected.id === item.id);
-			if (index === -1) {
-				this.selectedItems.push(item);
-			} else {
-				this.selectedItems.splice(index, 1);
-			}
-		}
-	}
-
-	delete(id: number) {
+	delete(id: number): void {
 		this.materialService.delete(id).subscribe({
 			next: () => {
 				this.loadData();
-				this.utilsService.showSuccessMessage("Material removido com sucesso!")
+				this.utilsService.showSuccessMessage("Material removido com sucesso!");
 			}
 		});
 	}
 
-	openAddDialog() {
+	openAddDialog(): void {
 		const dialogRef = this.dialog.open(MaterialCreateComponent, {
 			width: '90vw',
 			maxWidth: '600px',
