6f815cf896c64720469b6c0e4a97acc84def6bc7
Diego Priess <diegopriess.dev@gmail.com>
2024-11-24 18:43:30 -0300

    Utilizando environments

--- src/app/config/utils.ts
+++ src/app/config/utils.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
+import { environment } from '../../environments/environments';
+
+export const BASE_URL = environment.base_url;
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    base_url: 'https://54.210.117.106'
+  };
